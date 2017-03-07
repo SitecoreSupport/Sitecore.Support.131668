131a7ba06a5b9d834d1bb2f679bb50b8ada550a9
DK\msb <msb@sitecore.net>
2017-03-07 13:27:36 -0500

    Configuration file has bene added

--- src/Sitecore.Support.131668/Sitecore.Support.131668/Shell/Framework/Commands/ContentEditor/ShowWorkflowCommands.cs
+++ src/Sitecore.Support.131668/Sitecore.Support.131668/Shell/Framework/Commands/ContentEditor/ShowWorkflowCommands.cs
@@ -11,51 +11,54 @@ using Sitecore.Web.UI.Sheer;
 using Sitecore.Workflows;
 using System;
 
-[Serializable]
-public class ShowWorkflowCommands : Command
+namespace Sitecore.Support.Shell.Framework.Commands.ContentEditor
 {
-    // Methods
-    public override void Execute(CommandContext context)
+    [Serializable]
+    public class ShowWorkflowCommands : Command
     {
-        Assert.ArgumentNotNull(context, "context");
-        string name = context.Parameters["database"];
-        string path = context.Parameters["id"];
-        string str3 = context.Parameters["language"];
-        string str4 = context.Parameters["version"];
-        Database database = Factory.GetDatabase(name);
-        if (database != null)
+        // Methods
+        public override void Execute(CommandContext context)
         {
-            Item item = database.GetItem(path, Language.Parse(str3), Sitecore.Data.Version.Parse(str4));
-            if (item != null)
+            Assert.ArgumentNotNull(context, "context");
+            string name = context.Parameters["database"];
+            string path = context.Parameters["id"];
+            string str3 = context.Parameters["language"];
+            string str4 = context.Parameters["version"];
+            Database database = Factory.GetDatabase(name);
+            if (database != null)
             {
-                IWorkflow workflow = item.Database.WorkflowProvider.GetWorkflow(item);
-                if (workflow != null)
+                Item item = database.GetItem(path, Language.Parse(str3), Sitecore.Data.Version.Parse(str4));
+                if (item != null)
                 {
-                    WorkflowCommand[] commandArray = WorkflowFilterer.FilterVisibleCommands(workflow.GetCommands(item), item);
-                    if ((commandArray != null) && (commandArray.Length != 0))
+                    IWorkflow workflow = item.Database.WorkflowProvider.GetWorkflow(item);
+                    if (workflow != null)
                     {
-                        Menu contextMenu = new Menu();
-                        SheerResponse.DisableOutput();
-                        foreach (WorkflowCommand command in commandArray)
+                        WorkflowCommand[] commandArray = WorkflowFilterer.FilterVisibleCommands(workflow.GetCommands(item), item);
+                        if ((commandArray != null) && (commandArray.Length != 0))
                         {
-                            string click = new WorkflowCommandBuilder(item, workflow, command).ToString();
-                            contextMenu.Add("C" + command.CommandID, command.DisplayName, command.Icon, string.Empty, click, false, string.Empty, MenuItemType.Normal).Disabled = !Context.User.IsAdministrator && !item.Locking.HasLock()&& Settings.RequireLockBeforeEditing;
+                            Menu contextMenu = new Menu();
+                            SheerResponse.DisableOutput();
+                            foreach (WorkflowCommand command in commandArray)
+                            {
+                                string click = new WorkflowCommandBuilder(item, workflow, command).ToString();
+                                contextMenu.Add("C" + command.CommandID, command.DisplayName, command.Icon, string.Empty, click, false, string.Empty, MenuItemType.Normal).Disabled = !Context.User.IsAdministrator && !item.Locking.HasLock() && Settings.RequireLockBeforeEditing;
+                            }
+                            SheerResponse.EnableOutput();
+                            SheerResponse.ShowContextMenu(Context.ClientPage.ClientRequest.Control, "right", contextMenu);
                         }
-                        SheerResponse.EnableOutput();
-                        SheerResponse.ShowContextMenu(Context.ClientPage.ClientRequest.Control, "right", contextMenu);
                     }
                 }
             }
         }
-    }
 
-    public override CommandState QueryState(CommandContext context)
-    {
-        if (!Settings.Workflows.Enabled)
+        public override CommandState QueryState(CommandContext context)
         {
-            return CommandState.Hidden;
+            if (!Settings.Workflows.Enabled)
+            {
+                return CommandState.Hidden;
+            }
+            return base.QueryState(context);
         }
-        return base.QueryState(context);
     }
-}
 
+}
